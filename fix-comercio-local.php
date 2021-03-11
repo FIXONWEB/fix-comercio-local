d28605cf2fe97515318d5de3cfd55a08276882fb
fixonweb <contato@fixonweb.com.br>
2021-03-11 16:35:15 -0300

    0.1.27

--- fix-comercio-local.php
+++ fix-comercio-local.php
@@ -7,7 +7,7 @@
  * Author URI:      https://fixonweb.com.br
  * Text Domain:     fix-comercio-local
  * Domain Path:     /languages
- * Version:         0.1.26
+ * Version:         0.1.27
  *
  * @package         Fix_Comercio_Local
  */
