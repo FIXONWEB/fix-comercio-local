460214d904eafabd3a8b26a360f05888d44f88b8
fixonweb <contato@fixonweb.com.br>
2021-03-11 16:28:21 -0300

    0.1.23

--- fix-comercio-local.php
+++ fix-comercio-local.php
@@ -7,7 +7,7 @@
  * Author URI:      https://fixonweb.com.br
  * Text Domain:     fix-comercio-local
  * Domain Path:     /languages
- * Version:         0.1.22
+ * Version:         0.1.23
  *
  * @package         Fix_Comercio_Local
  */
--- includes/single_template.php
+++ includes/single_template.php
@@ -5,6 +5,12 @@ add_filter('single_template', 'fix161174_template');
 function fix161174_template($single) {
 	global $post;
 
+	if ($post->post_type == 'comercio-local') {
+		$fix_dir_path = plugin_dir_path( __FILE__ );
+		die($fix_dir_path);
+        // $single_template = 'single-comercio-local.php'; 
+
+    }
 
 	return $single;
 }
