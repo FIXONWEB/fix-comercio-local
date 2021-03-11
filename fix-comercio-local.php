747bce03bc0771d469c14077472bd45cc2f3873c
fixonweb <contato@fixonweb.com.br>
2021-03-11 11:38:41 -0300

    0.1.7

--- fix-comercio-local.php
+++ fix-comercio-local.php
@@ -7,7 +7,7 @@
  * Author URI:      https://fixonweb.com.br
  * Text Domain:     fix-comercio-local
  * Domain Path:     /languages
- * Version:         0.1.6
+ * Version:         0.1.7
  *
  * @package         Fix_Comercio_Local
  */
@@ -19,7 +19,8 @@ $fix1608230887_url_update 	= 'https://github.com/fixonweb/fix-comercio-local';
 $fix1608230887_slug 		= 'fix-comercio-local/fix-comercio-local';
 $fix1608230887_check 		= Puc_v4_Factory::buildUpdateChecker($fix1608230887_url_update,__FILE__,$fix1608230887_slug);
 
-include "includes/parse_request.php";
+include WP_PLUGIN_DIR."/includes/parse_request.php";
+
 
 register_activation_hook( __FILE__, 'fix161174_activation_hook' );
 function fix161174_activation_hook() {
